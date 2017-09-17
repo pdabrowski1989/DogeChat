42f4db83b5f22bd8188a5a5cb7b236278823c40d
pdabrowski1989 <pawel.dabrowski1989@gmail.com>
2017-09-17 20:24:45 +0200

    webpack init

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,3 +1,35 @@
-/**
- * Created by PawelD on 2017-09-17.
- */
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: '',
+        vendor: ''
+    },
+    output: {
+        path: path.resolve("../dist"),
+        filename: "[filename].min.js"
+    },
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env'],
+                    plugins: ['transform-runtime']
+                }
+            }
+        }
+    ],
+    devtool: "source-map",
+    plugins: [
+        new UglifyJsPlugin({
+            compress: {
+                warnings: true
+            }
+        })
+    ]
+};
