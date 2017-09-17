f77575bd6beece9afaf7e77d5762908338cb02ad
pdabrowski1989 <pawel.dabrowski1989@gmail.com>
2017-09-17 21:14:16 +0200

    gulp js serve

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,8 @@ const sass = require('gulp-sass');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const webserver = require('gulp-webserver');
+
 
 gulp.task('saas', () => {
     return gulp.src('./app/**/*.scss')
@@ -17,4 +19,19 @@ gulp.task('js', () => {
         .pipe(concat('app.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist'))
+});
+
+gulp.task('build', ['js', 'saas']);
+
+gulp.task('watch', () => {
+    return gulp.watch(['./index.html', './app/**/*.html', './app/**/*.scss', './app/**/*.js'], ['build']);
+});
+
+gulp.task('serve', ['watch', 'build'], () => {
+    gulp.src('.')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: true,
+            open: "http://localhost:8000/dist/index.html"
+        }));
 });
